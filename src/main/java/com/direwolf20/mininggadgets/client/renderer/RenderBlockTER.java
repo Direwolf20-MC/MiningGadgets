3cb20f1c5ba232abfc53737aae19ecdbdf8f9bd1
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2019-10-08 10:42:15 -0400

    Fading blocks work now, yay!

--- src/main/java/com/direwolf20/mininggadgets/client/renderer/RenderBlockTER.java
+++ src/main/java/com/direwolf20/mininggadgets/client/renderer/RenderBlockTER.java
@@ -1,5 +1,6 @@
 package com.direwolf20.mininggadgets.client.renderer;
 
+import com.direwolf20.mininggadgets.common.blocks.RenderBlock;
 import com.direwolf20.mininggadgets.common.tiles.RenderBlockTileEntity;
 import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.block.BlockState;
@@ -7,11 +8,42 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.model.BakedQuad;
+import net.minecraft.client.renderer.model.IBakedModel;
 import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.Vec3i;
+import org.lwjgl.opengl.GL14;
+
+import java.util.List;
+import java.util.Random;
 
 public class RenderBlockTER extends TileEntityRenderer<RenderBlockTileEntity> {
     public RenderBlockTER() {
+    }
+
+    private void renderModelBrightnessColorQuads(float brightness, float red, float green, float blue, List<BakedQuad> listQuads) {
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        int i = 0;
+
+        for (int j = listQuads.size(); i < j; ++i) {
+            BakedQuad bakedquad = listQuads.get(i);
+            bufferbuilder.begin(7, DefaultVertexFormats.ITEM);
+            bufferbuilder.addVertexData(bakedquad.getVertexData());
+            if (bakedquad.hasTintIndex()) {
+                bufferbuilder.putColorRGB_F4(red * brightness, green * brightness, blue * brightness);
+            } else {
+                bufferbuilder.putColorRGB_F4(brightness, brightness, brightness);
+            }
+
+            Vec3i vec3i = bakedquad.getFace().getDirectionVec();
+            bufferbuilder.putNormal((float) vec3i.getX(), (float) vec3i.getY(), (float) vec3i.getZ());
+            tessellator.draw();
+        }
+
     }
 
     @Override
@@ -20,8 +52,11 @@ public class RenderBlockTER extends TileEntityRenderer<RenderBlockTileEntity> {
 
         BlockRendererDispatcher blockrendererdispatcher = Minecraft.getInstance().getBlockRendererDispatcher();
         Minecraft mc = Minecraft.getInstance();
-        GlStateManager.pushMatrix();
         mc.getTextureManager().bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+        GlStateManager.pushMatrix();
+        GlStateManager.enableBlend();
+        //This blend function allows you to use a constant alpha, which is defined later
+        GlStateManager.blendFunc(GL14.GL_CONSTANT_ALPHA, GL14.GL_ONE_MINUS_CONSTANT_ALPHA);
 
         int durability = tile.getDurability();
         int originalDurability = tile.getOriginalDurability();
@@ -32,14 +67,24 @@ public class RenderBlockTER extends TileEntityRenderer<RenderBlockTileEntity> {
             scale = 0;
         float trans = (1 - scale) / 2;
 
+        //todo Have the gadget have an option for fading/shrinking blocks in the table.
 
         GlStateManager.translated(x, y, z);
-        GlStateManager.translatef(trans, trans, trans);
+        //GlStateManager.translatef((1-0.99f)/2, (1-0.99f)/2, (1-0.99f)/2);
         GlStateManager.rotatef(-90.0F, 0.0F, 1.0F, 0.0F);
-        GlStateManager.scalef(scale, scale, scale);
-
+        //GlStateManager.scalef(0.99f, 0.99f, 0.99f);
+        GL14.glBlendColor(1F, 1F, 1F, scale); //Set the alpha of the blocks we are rendering
         try {
-            blockrendererdispatcher.renderBlockBrightness(renderState, 1.0f);
+            IBakedModel ibakedmodel = blockrendererdispatcher.getModelForState(renderState);
+            GlStateManager.rotatef(90.0F, 0.0F, 1.0F, 0.0F);
+            Random random = new Random();
+            random.setSeed(42L);
+            //blockrendererdispatcher.renderBlockBrightness(renderState, 1.0f);
+            for (Direction direction : Direction.values()) {
+                if (!(getWorld().getBlockState(tile.getPos().offset(direction)).getBlock() instanceof RenderBlock)) {
+                    renderModelBrightnessColorQuads(1f, 1f, 1f, 1f, ibakedmodel.getQuads(renderState, direction, random));
+                }
+            }
         } catch (Throwable t) {
             Tessellator tessellator = Tessellator.getInstance();
             BufferBuilder bufferBuilder = tessellator.getBuffer();
@@ -51,6 +96,75 @@ public class RenderBlockTER extends TileEntityRenderer<RenderBlockTileEntity> {
 
             }
         }
+        //Disable blend
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+        GlStateManager.disableBlend();
         GlStateManager.popMatrix();
+/*
+
+        GlStateManager.enableBlend();
+        GlStateManager.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        GlStateManager.disableTexture();
+        //GlStateManager.depthMask(false);
+        Tessellator t = Tessellator.getInstance();
+        BufferBuilder bufferBuilder = t.getBuffer();
+        bufferBuilder.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_COLOR);
+
+        double maxX = x + 1;
+        double maxY = y + 1;
+        double maxZ = z + 1;
+        float red = 1f;
+        float green = 0.0f;
+        float blue = 0.0f;
+        float alpha = (1f - (scale));
+        if (alpha < 0.051f) {
+            alpha = 0.051f;
+        }
+        if (alpha > 0.33f) {
+            alpha = 0.33f;
+        }
+        alpha = 0.33f;
+        // Down
+        bufferBuilder.pos(x, y, z).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, y, z).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, y, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(x, y, maxZ).color(red, green, blue, alpha).endVertex();
+
+        // Up
+        bufferBuilder.pos(x, maxY, z).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(x, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, maxY, z).color(red, green, blue, alpha).endVertex();
+
+        // North
+        bufferBuilder.pos(x, y, z).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(x, maxY, z).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, maxY, z).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, y, z).color(red, green, blue, alpha).endVertex();
+
+        // South
+        bufferBuilder.pos(x, y, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, y, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(x, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+
+        // East
+        bufferBuilder.pos(maxX, y, z).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, maxY, z).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, y, maxZ).color(red, green, blue, alpha).endVertex();
+
+        // West
+        bufferBuilder.pos(x, y, z).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(x, y, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(x, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(x, maxY, z).color(red, green, blue, alpha).endVertex();
+        t.draw();
+
+        GlStateManager.disableBlend();
+        GlStateManager.enableTexture();
+        //GlStateManager.depthMask(true);
+*/
+
     }
 }
