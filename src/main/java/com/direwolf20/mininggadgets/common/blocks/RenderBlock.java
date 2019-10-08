fa19e36ea5ff26bac13407add527aec82b2b6178
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2019-10-07 20:34:57 -0400

    Remove Debug Code

--- src/main/java/com/direwolf20/mininggadgets/common/blocks/RenderBlock.java
+++ src/main/java/com/direwolf20/mininggadgets/common/blocks/RenderBlock.java
@@ -4,19 +4,12 @@ import com.direwolf20.mininggadgets.common.tiles.RenderBlockTileEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.PushReaction;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.UseAction;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.World;
 import net.minecraft.world.storage.loot.LootContext;
 
 import javax.annotation.Nullable;
@@ -31,7 +24,7 @@ public class RenderBlock extends Block {
         setRegistryName("renderblock");
     }
 
-    @Override
+    /*@Override
     public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
         if (!worldIn.isRemote) {
             TileEntity tileEntity = worldIn.getTileEntity(pos);
@@ -40,7 +33,7 @@ public class RenderBlock extends Block {
             }
         }
         return true;
-    }
+    }*/
 
     @Override
     public boolean hasTileEntity(BlockState state) {
