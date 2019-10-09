0f233023ae2fbdad5662e0275484f1753cd13c51
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2019-10-08 20:47:42 -0400

    Minor Render Tweaks

--- src/main/java/com/direwolf20/mininggadgets/client/renderer/RenderBlockTER.java
+++ src/main/java/com/direwolf20/mininggadgets/client/renderer/RenderBlockTER.java
@@ -71,14 +71,12 @@ public class RenderBlockTER extends TileEntityRenderer<RenderBlockTileEntity> {
 
         GlStateManager.translated(x, y, z);
         //GlStateManager.translatef((1-0.99f)/2, (1-0.99f)/2, (1-0.99f)/2);
-        GlStateManager.rotatef(-90.0F, 0.0F, 1.0F, 0.0F);
+        //GlStateManager.rotatef(-90.0F, 0.0F, 1.0F, 0.0F);
         //GlStateManager.scalef(0.99f, 0.99f, 0.99f);
         GL14.glBlendColor(1F, 1F, 1F, scale); //Set the alpha of the blocks we are rendering
         try {
             IBakedModel ibakedmodel = blockrendererdispatcher.getModelForState(renderState);
-            GlStateManager.rotatef(90.0F, 0.0F, 1.0F, 0.0F);
-            Random random = new Random();
-            random.setSeed(42L);
+            Random random = new Random(42L);
             //blockrendererdispatcher.renderBlockBrightness(renderState, 1.0f);
             for (Direction direction : Direction.values()) {
                 if (!(getWorld().getBlockState(tile.getPos().offset(direction)).getBlock() instanceof RenderBlock)) {
