7ad6c8bd49e0679a2cff4620f8e3965262025de2
Michael Hillcox <hello.michaelhillcox@gmail.com>
2019-10-25 23:47:57 +0100

    Fixed upgrade ordering issues

--- src/main/java/com/direwolf20/mininggadgets/common/items/ModItems.java
+++ src/main/java/com/direwolf20/mininggadgets/common/items/ModItems.java
@@ -25,24 +25,25 @@ public class ModItems {
      * Upgrades are a bit ugly.. Soz
      * This one is actually kinda
      */
+
     public static final RegistryObject<Item> UPGRADE_EMPTY =    UPGRADE_ITEMS.register("upgrade_empty", Upgrade.EMPTY::getCard);
     public static final RegistryObject<Item> SILK =             UPGRADE_ITEMS.register("upgrade_silk", Upgrade.SILK::getCard);
-    public static final RegistryObject<Item> FREEZING =         UPGRADE_ITEMS.register("upgrade_freezing", Upgrade.VOID_JUNK::getCard);
-    public static final RegistryObject<Item> FORTUNE_1 =        UPGRADE_ITEMS.register("upgrade_fortune_1", Upgrade.MAGNET::getCard);
-    public static final RegistryObject<Item> FORTUNE_2 =        UPGRADE_ITEMS.register("upgrade_fortune_2", Upgrade.THREE_BY_THREE::getCard);
-    public static final RegistryObject<Item> FORTUNE_3 =        UPGRADE_ITEMS.register("upgrade_fortune_3", Upgrade.LIGHT_PLACER::getCard);
-    public static final RegistryObject<Item> LIGHT_PLACER =     UPGRADE_ITEMS.register("upgrade_light_placer", Upgrade.FREEZING::getCard);
-    public static final RegistryObject<Item> THREE_BY_THREE =   UPGRADE_ITEMS.register("upgrade_three_by_three", Upgrade.FORTUNE_1::getCard);
-    public static final RegistryObject<Item> VOID_JUNK =        UPGRADE_ITEMS.register("upgrade_void_junk", Upgrade.FORTUNE_2::getCard);
-    public static final RegistryObject<Item> MAGNET =           UPGRADE_ITEMS.register("upgrade_magnet", Upgrade.FORTUNE_3::getCard);
-    public static final RegistryObject<Item> EFFICIENCY_1 =     UPGRADE_ITEMS.register("upgrade_efficiency_1", Upgrade.BATTERY_1::getCard);
-    public static final RegistryObject<Item> EFFICIENCY_2 =     UPGRADE_ITEMS.register("upgrade_efficiency_2", Upgrade.BATTERY_2::getCard);
-    public static final RegistryObject<Item> EFFICIENCY_3 =     UPGRADE_ITEMS.register("upgrade_efficiency_3", Upgrade.BATTERY_3::getCard);
-    public static final RegistryObject<Item> EFFICIENCY_4 =     UPGRADE_ITEMS.register("upgrade_efficiency_4", Upgrade.EFFICIENCY_1::getCard);
-    public static final RegistryObject<Item> EFFICIENCY_5 =     UPGRADE_ITEMS.register("upgrade_efficiency_5", Upgrade.EFFICIENCY_2::getCard);
-    public static final RegistryObject<Item> BATTERY_1 =        UPGRADE_ITEMS.register("upgrade_battery_1", Upgrade.EFFICIENCY_3::getCard);
-    public static final RegistryObject<Item> BATTERY_2 =        UPGRADE_ITEMS.register("upgrade_battery_2", Upgrade.EFFICIENCY_4::getCard);
-    public static final RegistryObject<Item> BATTERY_3 =        UPGRADE_ITEMS.register("upgrade_battery_3", Upgrade.EFFICIENCY_5::getCard);
+    public static final RegistryObject<Item> VOID_JUNK =        UPGRADE_ITEMS.register("upgrade_void_junk", Upgrade.VOID_JUNK::getCard);
+    public static final RegistryObject<Item> MAGNET =           UPGRADE_ITEMS.register("upgrade_magnet", Upgrade.MAGNET::getCard);
+    public static final RegistryObject<Item> THREE_BY_THREE =   UPGRADE_ITEMS.register("upgrade_three_by_three", Upgrade.THREE_BY_THREE::getCard);
+    public static final RegistryObject<Item> LIGHT_PLACER =     UPGRADE_ITEMS.register("upgrade_light_placer", Upgrade.LIGHT_PLACER::getCard);
+    public static final RegistryObject<Item> FREEZING =         UPGRADE_ITEMS.register("upgrade_freezing", Upgrade.FREEZING::getCard);
+    public static final RegistryObject<Item> FORTUNE_1 =        UPGRADE_ITEMS.register("upgrade_fortune_1", Upgrade.FORTUNE_1::getCard);
+    public static final RegistryObject<Item> FORTUNE_2 =        UPGRADE_ITEMS.register("upgrade_fortune_2", Upgrade.FORTUNE_2::getCard);
+    public static final RegistryObject<Item> FORTUNE_3 =        UPGRADE_ITEMS.register("upgrade_fortune_3", Upgrade.FORTUNE_3::getCard);
+    public static final RegistryObject<Item> BATTERY_1 =        UPGRADE_ITEMS.register("upgrade_battery_1", Upgrade.BATTERY_1::getCard);
+    public static final RegistryObject<Item> BATTERY_2 =        UPGRADE_ITEMS.register("upgrade_battery_2", Upgrade.BATTERY_2::getCard);
+    public static final RegistryObject<Item> BATTERY_3 =        UPGRADE_ITEMS.register("upgrade_battery_3", Upgrade.BATTERY_3::getCard);
+    public static final RegistryObject<Item> EFFICIENCY_1 =     UPGRADE_ITEMS.register("upgrade_efficiency_1", Upgrade.EFFICIENCY_1::getCard);
+    public static final RegistryObject<Item> EFFICIENCY_2 =     UPGRADE_ITEMS.register("upgrade_efficiency_2", Upgrade.EFFICIENCY_2::getCard);
+    public static final RegistryObject<Item> EFFICIENCY_3 =     UPGRADE_ITEMS.register("upgrade_efficiency_3", Upgrade.EFFICIENCY_3::getCard);
+    public static final RegistryObject<Item> EFFICIENCY_4 =     UPGRADE_ITEMS.register("upgrade_efficiency_4", Upgrade.EFFICIENCY_4::getCard);
+    public static final RegistryObject<Item> EFFICIENCY_5 =     UPGRADE_ITEMS.register("upgrade_efficiency_5", Upgrade.EFFICIENCY_5::getCard);
 
     // Block items
     public static final RegistryObject<Item> MODIFICATION_TABLE_ITEM = ITEMS.register("modificationtable", () -> new BlockItem(ModBlocks.MODIFICATION_TABLE.get(), ITEM_GROUP));
