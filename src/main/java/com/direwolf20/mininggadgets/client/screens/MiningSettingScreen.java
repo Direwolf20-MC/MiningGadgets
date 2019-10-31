9ac8ce0e880e8729b36273f89ecef84dcd45ed48
Michael Hillcox <hello.michaelhillcox@gmail.com>
2019-10-31 21:55:27 +0000

    Added pretty toggles

--- src/main/java/com/direwolf20/mininggadgets/client/screens/MiningSettingScreen.java
+++ src/main/java/com/direwolf20/mininggadgets/client/screens/MiningSettingScreen.java
@@ -6,24 +6,15 @@ import com.direwolf20.mininggadgets.common.gadget.upgrade.UpgradeTools;
 import com.direwolf20.mininggadgets.common.network.PacketHandler;
 import com.direwolf20.mininggadgets.common.network.Packets.PacketUpdateUpgrade;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.StringTextComponent;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MiningSettingScreen extends Screen {
-    // The upgrades you're allowed to disable and enable
-    private static final Collection<String> allowedDisables = Arrays.asList(
-            Upgrade.FREEZING.getName(),
-            Upgrade.SILK.getName(),
-            Upgrade.VOID_JUNK.getName(),
-            Upgrade.MAGNET.getName(),
-            Upgrade.LIGHT_PLACER.getName()
-    );
-
     private ItemStack gadget;
     private List<Upgrade> gadgetUpgrades = new ArrayList<>();
 
@@ -37,13 +28,16 @@ public class MiningSettingScreen extends Screen {
     @Override
     protected void init() {
         int x = 0;
-        for (Upgrade upgrade : this.gadgetUpgrades) {
-            addButton(new ToggleButton((width / 2) - 50, ((height / 2) - 80) + (x * 23), 50, upgrade.getName(), (e) -> {
-                if( e ) {
+        List<Upgrade> toggleableList = this.gadgetUpgrades.stream().filter(Upgrade::isToggleable).collect(Collectors.toList());
+        for (Upgrade upgrade : toggleableList) {
+            addButton(new ToggleButton((width / 2) - (160 / 2), ((height / 2) - ((toggleableList.size() * 25) / 2)) + (x * 25), 50, I18n.format(upgrade.getLocal()).replace(I18n.format(upgrade.getLocalReplacement()), ""), shouldUpdate -> {
+                // When the button is clicked we toggle
+                if( shouldUpdate ) {
                     upgrade.setEnabled(!upgrade.isEnabled());
                     updateUpgrade(upgrade);
                 }
 
+                // When we're just init the gui, we check if it's on or off.
                 return upgrade.isEnabled();
             }));
 
--- src/main/java/com/direwolf20/mininggadgets/client/screens/widget/ToggleButton.java
+++ src/main/java/com/direwolf20/mininggadgets/client/screens/widget/ToggleButton.java
@@ -14,7 +14,8 @@ public class ToggleButton extends Widget {
     public ToggleButton(int xIn, int yIn, int width, String msg, Predicate<Boolean> onPress) {
         super(xIn, yIn, msg);
 
-        this.width = 200;
+        this.width = 160;
+        this.height = 18;
         this.onPress = onPress;
 
         this.enabled = this.onPress.test(false);
@@ -22,17 +23,23 @@ public class ToggleButton extends Widget {
 
     @Override
     public void renderButton(int p_renderButton_1_, int p_renderButton_2_, float p_renderButton_3_) {
-//        super.renderButton(p_renderButton_1_, p_renderButton_2_, p_renderButton_3_);
-
         GlStateManager.disableTexture();
         GlStateManager.color4f(0, 0, 0, 1f);
-        this.blit(this.x, this.y, 0, 0, 40, 22);
+        this.blit(this.x, this.y, 0, 0, this.width, this.height);
 
-        GlStateManager.color4f(1f, 1f, 1f, 1f);
-        this.blit(this.enabled ? this.x + 20 : this.x, this.y, 0, 0, 20, 20);
+        int right = this.x + this.width - 2;
+        GlStateManager.color4f(.2f, .2f, .2f, 1f);
+        this.blit(right - 29, this.y + 1, 0, 0, 30, this.height - 2);
+
+        if( this.enabled )
+            GlStateManager.color4f(0f, 1f, 0f, 1f);
+        else
+            GlStateManager.color4f(1f, 0f, 0f, 1f);
+
+        this.blit(this.enabled ? right - 10 : right - 28, this.y + 2, 0, 0, 10, this.height - 4);
         GlStateManager.enableTexture();
 
-        drawString(Minecraft.getInstance().fontRenderer, getMessage(), this.x, this.y, Color.WHITE.getRGB());
+        drawString(Minecraft.getInstance().fontRenderer, getMessage(), this.x + 6, this.y + (this.height / 2) - 4, Color.WHITE.getRGB());
     }
 
     @Override
