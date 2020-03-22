aaf92eff211ad51f5d471583caf882f7c13130c6
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2020-03-21 22:35:12 -0400

    Check for adjacent storage, probably a better way?

--- src/main/java/com/direwolf20/mininggadgets/common/blocks/Quarry.java
+++ src/main/java/com/direwolf20/mininggadgets/common/blocks/Quarry.java
@@ -4,9 +4,13 @@ import com.direwolf20.mininggadgets.common.tiles.QuarryBlockTileEntity;
 import com.direwolf20.mininggadgets.common.tiles.RenderBlockTileEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 
@@ -27,4 +31,10 @@ public class Quarry extends Block {
         return new QuarryBlockTileEntity();
     }
 
+    @Override
+    public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
+        super.neighborChanged(state, worldIn, pos, blockIn, fromPos, isMoving);
+        QuarryBlockTileEntity te = (QuarryBlockTileEntity) worldIn.getTileEntity(pos);
+        te.scanAdjacentStorage();
+    }
 }
--- src/main/java/com/direwolf20/mininggadgets/common/tiles/QuarryBlockTileEntity.java
+++ src/main/java/com/direwolf20/mininggadgets/common/tiles/QuarryBlockTileEntity.java
@@ -1,15 +1,45 @@
 package com.direwolf20.mininggadgets.common.tiles;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.ArrayList;
 
 import static com.direwolf20.mininggadgets.common.blocks.ModBlocks.QUARRY_TILE;
 
 public class QuarryBlockTileEntity extends TileEntity implements ITickableTileEntity {
 
+    public ArrayList<BlockPos> adjacentStorage;
+    boolean needScanAdjacent;
+
     public QuarryBlockTileEntity() {
         super(QUARRY_TILE.get());
+        needScanAdjacent = true;
+    }
+
+    public void addToAdjacentStorage(BlockPos pos) {
+        adjacentStorage.add(pos);
+    }
+
+    public void removeFromAdjacentStorage(BlockPos pos) {
+        adjacentStorage.remove(pos);
+    }
+
+    public void scanAdjacentStorage() {
+        adjacentStorage = new ArrayList<BlockPos>();
+        for (Direction direction : Direction.values()) {
+            BlockPos adjacentPos = this.pos.offset(direction);
+            BlockState adjacentState = world.getBlockState(adjacentPos);
+            if (adjacentState.getBlock().equals(Blocks.CHEST)) { //TODO Look for any inventory
+                adjacentStorage.add(this.pos.offset(direction));
+            }
+        }
+        needScanAdjacent = false;
     }
 
     @Override
@@ -28,8 +58,9 @@ public class QuarryBlockTileEntity extends TileEntity implements ITickableTileEn
 
     @Override
     public void tick() {
+        if (needScanAdjacent) scanAdjacentStorage();
         if (!world.isRemote) {
-          
+            System.out.println(adjacentStorage);
         }
     }
 }
